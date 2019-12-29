8b3cdfca1d93752981efb2b58b740b2384f58bce
bfrasure <bill.frasure@gmail.com>
2019-12-29 16:47:38 -0700

    Clean up multiple unsafeRunSync calls :)

--- src/main/scala/service/LoginEndpoint.scala
+++ src/main/scala/service/LoginEndpoint.scala
@@ -14,7 +14,7 @@ class LoginEndpoint [F[_]: Sync](
                                   bearerTokenStore: BackingStore[IO, SecureRandomId, TSecBearerToken[Int]]
                                 )(
                                   implicit ev: Effect[F]
-                                ) extends Http4sDsl[IO] {
+                                ) extends Http4sDsl[F] {
   case class RequestWithToken(token: String)
   def contactOAuthProvider(userId: Int): RequestWithToken = {
     RequestWithToken(s"Fresh token for $userId")
@@ -28,20 +28,19 @@ class LoginEndpoint [F[_]: Sync](
       None
     )
   }
-  val service = HttpRoutes.of[IO] {
+  val service = HttpRoutes.of[F] {
     case GET -> Root / userId => {
       println(s"Attempting to login userId=$userId")
-      val result: String = userStore.get(userId.toInt).value.unsafeRunSync() match {
-        case None => {
-          val newUserResult = userStore.put(User(idInt = userId.toInt, name = s"name_$userId", age = 1)).unsafeRunSync()
-          val storedUser: User = userStore.get(userId.toInt).value.unsafeRunSync().get
-          val newBearerToken = generateNewTokenFor(storedUser)
-          val storedToken = bearerTokenStore.put(newBearerToken).unsafeRunSync()
-          storedToken.toString
-        }
+      val result = userStore.get(userId.toInt).value.unsafeRunSync() match {
+        case None =>
+          userStore.put(User(idInt = userId.toInt, name = s"name_$userId", age = 1))
+            .flatMap( newUserResult => {
+              val newBearerToken = generateNewTokenFor(newUserResult)
+              bearerTokenStore.put(newBearerToken)
+            })
+
         case Some(existingUser) => {
           val secureUserId = SecureRandomId.coerce(existingUser.idInt.toString)
-          val result =
             bearerTokenStore.get(secureUserId).map {
               existingToken =>
               if (existingToken.expiry.isBefore(Instant.now())) {// TODO Bad side effect
@@ -56,10 +55,9 @@ class LoginEndpoint [F[_]: Sync](
               println(s"Generating a new token: $newBearerToken")
               bearerTokenStore.put(newBearerToken)
             }
-          result.unsafeRunSync().toString
         }
       }
-      Ok(result)
+      Ok(result.unsafeRunSync().toString)
     }
   }
 
