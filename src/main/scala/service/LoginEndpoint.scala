9a8dd7712fe5e22d1d8bd9dbb1c894c270b6213c
bfrasure <bill.frasure@gmail.com>
2019-12-29 20:31:27 -0700

    More login decomposition

--- src/main/scala/service/LoginEndpoint.scala
+++ src/main/scala/service/LoginEndpoint.scala
@@ -30,35 +30,38 @@ class LoginEndpoint [F[_]: Sync](
       None
     )
   }
-  def loginLogic(userId: String) = userStore.get(userId.toInt).value.unsafeRunSync() match {
-    case None =>
-      userStore.put(User(idInt = userId.toInt, name = s"name_$userId", age = 1))
-        .flatMap( newUserResult =>
-          bearerTokenStore.put(generateNewTokenFor(newUserResult))
-        )
+  def createUserWithBearerToken(userId: String) =
+  userStore.put(User(idInt = userId.toInt, name = s"name_$userId", age = 1))
+    .flatMap( newUserResult =>
+      bearerTokenStore.put(generateNewTokenFor(newUserResult))
+    )
 
-    case Some(existingUser) =>
-      val secureUserId = SecureRandomId.coerce(existingUser.idInt.toString)
-      bearerTokenStore.get(secureUserId).map {
-        existingToken =>
-          if (existingToken.expiry.isBefore(Instant.now())) {// TODO Bad side effect
-            val newBearerToken = generateNewTokenFor(existingUser)
-            bearerTokenStore.update(newBearerToken)
-              .map(ignoredToken => s"User exists, but needed to replace an expired token")
-          } else {
-            ev.pure(s"User exists with a valid token")
-          }
-      }.getOrElseF {
-        val newBearerToken = generateNewTokenFor(existingUser)
-        println(s"Generating a new token: $newBearerToken")
-        bearerTokenStore.put(newBearerToken)
-      }
-
+  def createBearerTokenForExistingUserIfNeeded(existingUser: User) = {
+    val secureUserId = SecureRandomId.coerce(existingUser.idInt.toString)
+    bearerTokenStore.get(secureUserId).map {
+      existingToken =>
+        if (existingToken.expiry.isBefore(Instant.now())) { // TODO Bad side effect
+          val newBearerToken = generateNewTokenFor(existingUser)
+          bearerTokenStore.update(newBearerToken)
+            .map(ignoredToken => s"User exists, but needed to replace an expired token")
+        } else {
+          ev.pure(s"User exists with a valid token")
+        }
+    }.getOrElseF {
+      val newBearerToken = generateNewTokenFor(existingUser)
+      println(s"Generating a new token: $newBearerToken")
+      bearerTokenStore.put(newBearerToken)
+    }
   }
+  def loginLogicWithUserCreation(userId: String) =
+    userStore.get(userId.toInt).value.unsafeRunSync() match {
+      case None => createUserWithBearerToken(userId)
+      case Some(existingUser) => createBearerTokenForExistingUserIfNeeded(existingUser)
+    }
   val service = HttpRoutes.of[F] {
     case request @ GET -> Root / userId => {
       println(s"Attempting to login userId=$userId")
-      val result = loginLogic(userId)
+      val result = loginLogicWithUserCreation(userId)
       Ok(result.unsafeRunSync().toString)
     }
   }
