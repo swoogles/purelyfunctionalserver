6415d77ba16bfda22ca9f6146e8f9cefb3c9a287
bfrasure <bill.frasure@gmail.com>
2020-10-05 00:04:02 -0600

    Tinkering with different filled BTrees #time 10m

--- shared/src/test/scala/data_structures/BTreeTest.scala
+++ shared/src/test/scala/data_structures/BTreeTest.scala
@@ -4,7 +4,7 @@ import utest._
 
 object BTreeTest extends TestSuite {
   val bTree =
-    new BTree(Leaf, 3)
+    new BTree(Leaf, 4)
   val tests = Tests {
     test("insert", {
       pprint.pprintln(
@@ -17,6 +17,14 @@ object BTreeTest extends TestSuite {
           .insert(12)
           .insert(14)
           .insert(15)
+          .insert(5)
+          .insert(51)
+          .insert(33)
+          .insert(34)
+          .insert(36)
+          .insert(37)
+          .insert(38)
+          .insert(20)
       )
     })
   }
