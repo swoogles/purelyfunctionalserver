8b1ea5e00dd1761b4c0e6593f8a0f2f3ece2ac84
bfrasure <bill.frasure@gmail.com>
2020-11-11 21:39:11 -0700

    Create a decent Blockchain draft #time 1h 30m

--- shared/src/main/scala/data_structures/hash_tree/MerkleTree.scala
+++ shared/src/main/scala/data_structures/hash_tree/MerkleTree.scala
@@ -1,5 +1,7 @@
 package data_structures.hash_tree
 
+import java.time.Instant
+
 case class DataBlock(rawContent: String)
 
 case class Hash(value: String)
@@ -8,6 +10,12 @@ object Hash {
 
   def of(dataBlock: DataBlock): Hash =
     Hash(dataBlock.rawContent + "#")
+
+  def of(transaction: Transaction): Hash =
+    Hash("#" + transaction.acceptor.name + "->" + transaction.giver.name + "#")
+
+  def of(header: Header): Hash =
+    Hash("BLOCK:" + header.merkleRootHash)
 }
 
 trait HashTreeElement {
@@ -22,15 +30,89 @@ case class Leaf(dataBlock: DataBlock) extends HashTreeElement {
     Hash.of(dataBlock)
 }
 
+case class TransactionLeaf(transaction: Transaction) extends HashTreeElement {
+
+  val hash =
+    Hash.of(transaction)
+}
+
 // todo remove case
 case class Node(hash: Hash) extends HashTreeElement {}
 
 object Node {
 
-  def apply(children: List[Leaf]): Node =
+  def apply(children: List[HashTreeElement]): Node =
     Node(
-      Hash(children.map(_.hash).mkString(":"))
+      Hash(children.map(_.hash.value).mkString(":"))
     )
 }
 
-class MerkleTree {}
+object MerkleTree {
+
+  def apply(rawData: List[DataBlock]): HashTreeElement =
+    rawData match {
+      case head :: tail =>
+        tail.foldLeft[HashTreeElement](Leaf(head))(
+          (acc, nextBlock) => Node(List(acc, Leaf(nextBlock)))
+        )
+      case Nil => Node(Hash("EMPTY ROOT HASH"))
+    }
+
+  def ofTransactions(rawData: List[Transaction]): HashTreeElement =
+    rawData match {
+      case head :: tail =>
+        tail.foldLeft[HashTreeElement](TransactionLeaf(head))(
+          (acc, nextBlock) => Node(List(acc, TransactionLeaf(nextBlock)))
+        )
+      case Nil => Node(Hash("EMPTY ROOT HASH"))
+    }
+
+  // todo dunno that this is actually important
+//  def applyInPasses(transactions: List[Transaction]): Hash =
+//    transactions.grouped(2).map {
+//      case Nil => Nil
+//      case item1 :: item2 :: Nil => Node(List(TransactionLeaf(item1), TransactionLeaf(item2)))
+//      case item1 :: Nil => Node(List(TransactionLeaf(item1), TransactionLeaf(item1)))
+//    }
+
+}
+
+trait User {
+  def submitTransaction(dataBlock: DataBlock, lastKnownState: HashTreeElement): HashTreeElement
+}
+
+trait BlockChain {
+  def blocks: List[DataBlock]
+  def addBlock(dataBlock: DataBlock): BlockChain
+}
+
+case class Block(
+  header: Header,
+  transactions: List[Transaction]
+)
+
+object BlockChain {
+
+  def newChain(timestamp: Instant, transactions: List[Transaction]): Block =
+    Block(Header(None, MerkleTree.ofTransactions(transactions).hash, timestamp), transactions)
+
+  def linkedTo(previous: Block, timestamp: Instant, transactions: List[Transaction]) =
+    Block(Header(Some(Hash.of(previous.header)),
+                 MerkleTree.ofTransactions(transactions).hash,
+                 timestamp),
+          transactions)
+
+}
+
+case class Header(
+  previousBlockHash: Option[Hash],
+  merkleRootHash: Hash,
+  timestamp: Instant
+)
+
+case class Party(name: String)
+
+case class Transaction(
+  acceptor: Party,
+  giver: Party
+)
--- shared/src/test/scala/data_structures/hash_tree/HashTreeTest.scala
+++ shared/src/test/scala/data_structures/hash_tree/HashTreeTest.scala
@@ -1,15 +1,40 @@
 package data_structures.hash_tree
 
+import java.time.Instant
+
 import utest.{TestSuite, Tests, test}
 
 object HashTreeTest extends TestSuite {
   val tests = Tests {
     test("contains", {
 
-      pprint.pprintln(Node(List(
-        Leaf(DataBlock("b1")),
-        Leaf(DataBlock("b2")),
-      )))
+//      pprint.pprintln(Node(List(
+//        Leaf(DataBlock("b1")),
+//        Leaf(DataBlock("b2")),
+//      )))
+    })
+
+//    test("blockChain", {
+//      pprint.pprintln(
+//        MerkleTree.apply(List(DataBlock("b1"), DataBlock("b2"), DataBlock("b3")))
+//      )
+//    })
+
+    test("transactionHashes", {
+      pprint.pprintln(
+        MerkleTree.ofTransactions(List(Transaction(Party("A"), Party("B")))).hash
+      )
+    })
+
+    test("chain creation", {
+      val firstBlock =
+        BlockChain.newChain(Instant.now(), List(Transaction(Party("A"), Party("B"))))
+      pprint.pprintln(
+        BlockChain.linkedTo(firstBlock,
+          Instant.now().plusSeconds(3600), List(Transaction(Party("C"), Party("D")))
+        )
+      )
     })
   }
 }
+//
